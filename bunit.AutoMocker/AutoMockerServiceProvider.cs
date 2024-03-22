e832e91a3d5e38e7c7ee49692a793cb96e4d4876
Maarten <17746901+snerte@users.noreply.github.com>
2024-03-22 13:49:48 +0100

    Add AutoMockerServiceProvider and AutoMockerTestContext classes

---
+++ bunit.AutoMocker/AutoMockerServiceProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Snerte.bunit.AutoMocker;
+
+public class AutoMockerServiceProvider(Moq.AutoMock.AutoMocker mocker) : IServiceProvider
+{
+    private readonly Moq.AutoMock.AutoMocker _mocker = mocker;
+
+    public object GetService(Type serviceType)
+    {
+        return _mocker.Get(serviceType);
+    }
+}
---
+++ bunit.AutoMocker/AutoMockerTestContext.cs
@@ -0,0 +1,14 @@
+﻿using Bunit;
+using Moq.AutoMock;
+
+namespace Snerte.bunit.AutoMocker;
+
+public abstract class AutoMockerTestContext : TestContext
+{
+    internal readonly Moq.AutoMock.AutoMocker _mocker = new();
+
+    public AutoMockerTestContext()
+    {
+        Services.AddFallbackServiceProvider(new AutoMockerServiceProvider(_mocker));
+    }
+}
